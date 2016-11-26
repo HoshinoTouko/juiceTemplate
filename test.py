6e3a29e4b8945ece3058d61e1eec8f030998999c
Touko Hoshino <Touko Hoshino>
2016-11-26 13:20:42 +0800

    Update for test. - That's too difficult and not easy to understand... qwq

--- test.py
+++ test.py
@@ -1,8 +1,13 @@
 #coding=utf-8
 import main
-
 testText = """
-<h1> Test </h1>\n
+For test
+{{ for 1 to $i }}
+test\n
+{{ $test1 }}
+{{ end for }}
+
+Common test\n
 {{ $test1 $test2 }}\n
 {{ $test3 $testNone }}\n
 {{ $single }}
@@ -13,7 +18,8 @@ test.addItem("test1", "test1")
 test.addItem("test2", "TTT2")
 dic = {
     "test3": "TEST333",
-    "single": "SINGLE DOG"
+    "single": "SINGLE DOG",
+    "i": 9
 }
 test.addItems(dic)
 print test.parse()
